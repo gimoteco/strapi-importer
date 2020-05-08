1d0402f49dccd4c1ddd2985a26b7854d925cc886
Guilherme Ferreira <guilherme.ferreira@x-apps.com.br>
2020-05-08 10:11:04 -0400

    feat: download imgs

--- index.js
+++ index.js
@@ -12,9 +12,15 @@ import {
 } from "./utils";
 import postsQuery from "./queries/posts";
 import slugify from "slugify";
+import { Buffer } from "buffer";
+import { resolve as urlPathJoin } from "url";
+
+const strapiServer = "http://localhost:1337";
+const outputImageDir = pathJoin(__dirname, "images");
+const frontmatterImagePath = "../static/assets/img/";
 
 const client = new ApolloClient({
-  uri: "http://localhost:1337/graphql",
+  uri: `${strapiServer}/graphql`,
   fetch,
 });
 
@@ -28,6 +34,14 @@ async function getPosts() {
   return postsResponse.data.posts;
 }
 
+async function download(url, outputPath) {
+  const response = await fetch(url);
+  const buffer = await response.arrayBuffer();
+  await writeFileAsync(outputPath, Buffer.from(buffer));
+}
+
+const getFilenameFromUrl = (url) => url.split("/").pop();
+
 async function generateMarkdown(post) {
   const {
     title,
@@ -35,25 +49,37 @@ async function generateMarkdown(post) {
     slug,
     createdAt,
     category,
+    image: { url: imageUrl },
     metaTags: { description, keywords },
   } = post;
   const date = formatDate(createdAt);
   const newSlug = slugify(`${date} ${sanitizeSlug(slug)}`);
   const filepath = pathJoin(__dirname, "posts", `${newSlug}.md`);
+  const imageFilename = getFilenameFromUrl(imageUrl);
+
   const frontmatter = getFrontmatter({
     title,
     date: formatDatetime(createdAt),
     category,
     description,
+    image: pathJoin(frontmatterImagePath, imageFilename),
     keywords: sanitizeKeywords(keywords),
   });
 
-  await writeFileAsync(filepath, `${frontmatter}\n${text}`);
+  return Promise.all([
+    download(
+      urlPathJoin(strapiServer, imageUrl),
+      pathJoin(outputImageDir, imageFilename)
+    ),
+    writeFileAsync(filepath, `${frontmatter}\n${text}`),
+  ]);
 }
 
 async function main() {
+  console.time("Execution");
   const posts = await getPosts();
-  posts.forEach(generateMarkdown);
+  await Promise.all(posts.map(generateMarkdown));
+  console.timeEnd("Execution");
 }
 
 main();
--- queries/posts.js
+++ queries/posts.js
@@ -9,7 +9,7 @@ export default gql`
       updatedAt
       category
       image {
-        id
+        url
       }
       metaTags {
         description
